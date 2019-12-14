c10bbe73d1000df62fda5675123c6d9fc35a7e7b
SuryatejaS <sunkara.surya@marlabs.com>
2019-12-15 00:00:03 +0530

    miss commit

--- test.js
+++ test.js
@@ -1,9 +1,5 @@
 var a="helloo"
 var b="hiii"
 console.log(hello);
-<<<<<<< HEAD
 "new branchgit git ;merge te branch with master"
 "added new line to check the merge"
-=======
-new branchgit git ;go to newbranch
->>>>>>> master
